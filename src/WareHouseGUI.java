9d3defd86188015b801ff22490fe8094bc53e170
Thanawat Sonsanoy <118097228+jpjapan@users.noreply.github.com>
2023-05-07 14:50:54 +0700

    Update WareHouseGUI.java

--- src/WareHouseGUI.java
+++ src/WareHouseGUI.java
@@ -1,23 +1,18 @@
-
 import javax.swing.*;
 import java.awt.*;
-import javax.swing.table.DefaultTableModel;
-
 public class WareHouseGUI {
-
     //Declare an attribute
     private JFrame fr;
     private JPanel p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17;
     private JButton bn1, bn2, bn3, bn4;
-    private JTextField txtName, txtAmount, txtPrice, txtCode, txtTotalOfProducts;
-    private JLabel la1, la2, la3, la4, title1, la_TotalOfProducts;
+    private JTextField txtName, txtAmount, txtPrice, txtCode, txtDateOfTime;
+    private JLabel la1, la2, la3, la4, title1, la_DateOfTime;
     private static JTable table;
     private JScrollPane scrollPane;
-    private ProductTableModel tablemodel;
-
-    public WareHouseGUI() {
+    private ProductTableModel tablemodel; 
+    
+    public WareHouseGUI(){
         fr = new JFrame();
-        bn1 = new JButton();
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Set size of JFrame
         fr.setBounds(580, 250, 750, 500);
@@ -26,7 +21,6 @@ public class WareHouseGUI {
         p1.setLayout(new BorderLayout());
         p2 = new JPanel();
         p2.setLayout(new GridLayout(9, 1));
-        fr.add(p1);
         p3 = new JPanel();
         p3.setLayout(new FlowLayout());
         p4 = new JPanel();
@@ -57,32 +51,62 @@ public class WareHouseGUI {
         p16.setLayout(new FlowLayout());
         p17 = new JPanel();
         p17.setLayout(new FlowLayout());
-
+        
         //set textfield on JPanel
         txtName = new JTextField(15); //Name
         txtName.setFont(new Font("Angsana New", Font.BOLD, 18));
+        
         txtAmount = new JTextField(13); //Amount
         txtAmount.setFont(new Font("Angsana New", Font.BOLD, 18));
+        
         txtPrice = new JTextField(15); //Price
         txtPrice.setFont(new Font("Angsana New", Font.BOLD, 18));
+        
         txtCode = new JTextField(15); //Code
         txtCode.setFont(new Font("Angsana New", Font.BOLD, 18));
-        txtTotalOfProducts = new JTextField(10);
-        txtTotalOfProducts.setFont(new Font("Angsana New", Font.BOLD, 18));
-        txtTotalOfProducts.setEditable(false);
+        
+        txtDateOfTime = new JTextField(7);
+        txtDateOfTime.setFont(new Font("Angsana New", Font.BOLD, 18));
+        txtDateOfTime.setEditable(false);
+        
+        //Set JLabel
         title1 = new JLabel("WareHouse");
         title1.setFont(new Font("Angsana New", Font.BOLD, 70));
-        p7.add(title1);
-        la_TotalOfProducts = new JLabel("Total");
-        la_TotalOfProducts.setFont(new Font("Angsana New", Font.BOLD, 25));
+        
+        la_DateOfTime = new JLabel("Date");
+        la_DateOfTime.setFont(new Font("Angsana New", Font.BOLD, 25));
+        
         la1 = new JLabel("Name");
         la1.setFont(new Font("Angsana New", Font.BOLD, 25));
+        
         la2 = new JLabel("Amount");
         la2.setFont(new Font("Angsana New", Font.BOLD, 25));
+        
         la3 = new JLabel("Price");
         la3.setFont(new Font("Angsana New", Font.BOLD, 25));
+        
         la4 = new JLabel("Code");
         la4.setFont(new Font("Angsana New", Font.BOLD, 25));
+        
+        //Set JButton on JFrame
+        bn1 = new JButton("RECORD");
+        bn1.setFont(new Font("Arial", Font.PLAIN, 15));
+        bn1.setPreferredSize(new Dimension(150, 30));
+        
+        bn2 = new JButton("DELETE");
+        bn2.setFont(new Font("Arial", Font.PLAIN, 15));
+        bn2.setPreferredSize(new Dimension(150, 30));
+        
+        bn3 = new JButton("UPDATE");
+        bn3.setFont(new Font("Arial", Font.PLAIN, 15));
+        bn3.setPreferredSize(new Dimension(150, 30));
+        
+        bn4 = new JButton("RESET");
+        bn4.setFont(new Font("Arial", Font.PLAIN, 15));
+        bn4.setPreferredSize(new Dimension(150, 30));
+        
+        fr.add(p1);
+        p7.add(title1);
         p3.add(la1);
         p3.add(txtName);
         p4.add(la2);
@@ -95,25 +119,11 @@ public class WareHouseGUI {
         p2.add(p3);
         p2.add(p5);
         p2.add(p4);
-        p17.add(la_TotalOfProducts);
-        p17.add(txtTotalOfProducts);
+        p17.add(la_DateOfTime);
+        p17.add(txtDateOfTime);
         p9.add(p14);
         p9.add(p15);
         p9.add(p16);
-        //p9.add(p17);
-        //Set JButton on JFrame
-        bn1 = new JButton("RECORD");
-        bn1.setFont(new Font("Arial", Font.PLAIN, 15));
-        bn1.setPreferredSize(new Dimension(150, 30));
-        bn2 = new JButton("DELETE");
-        bn2.setFont(new Font("Arial", Font.PLAIN, 15));
-        bn2.setPreferredSize(new Dimension(150, 30));
-        bn3 = new JButton("UPDATE");
-        bn3.setFont(new Font("Arial", Font.PLAIN, 15));
-        bn3.setPreferredSize(new Dimension(150, 30));
-        bn4 = new JButton("RESET");
-        bn4.setFont(new Font("Arial", Font.PLAIN, 15));
-        bn4.setPreferredSize(new Dimension(150, 30));
         p10.add(bn1);
         p11.add(bn2);
         p12.add(bn3);
@@ -123,15 +133,19 @@ public class WareHouseGUI {
         p2.add(p11);
         p2.add(p12);
         p2.add(p13);
+        p9.add(p17);
+        
         //Add components on the JFrame
         p1.add(p2, BorderLayout.WEST);
         p1.add(p7, BorderLayout.NORTH);
         p1.add(p9, BorderLayout.SOUTH);
+        
         //Set scrollPane for JTable
         scrollPane = new JScrollPane();
         scrollPane.setBounds(245, 80, 390, 300);
         p1.add(scrollPane, BorderLayout.CENTER);
         table = new JTable();
+        
         //Set Model for Jtable(ProductTableModel)
         tablemodel = new ProductTableModel();
         table.setModel(tablemodel);
@@ -155,12 +169,12 @@ public class WareHouseGUI {
         return bn4;
     }
 
-    public JTextField getTxtTotalOfProducts() {
-        return txtTotalOfProducts;
+    public JTextField gettxtDateOfTime() {
+        return txtDateOfTime;
     }
 
-    public void setTxtTotalOfProducts(JTextField txtTotalOfProducts) {
-        this.txtTotalOfProducts = txtTotalOfProducts;
+    public void settxtDateOfTime(JTextField txtDateOfTime) {
+        this.txtDateOfTime = txtDateOfTime;
     }
 
     public JTextField gettxtName() {
@@ -206,7 +220,6 @@ public class WareHouseGUI {
     public ProductTableModel getTablemodel() {
         return tablemodel;
     }
-
     public static void main(String[] args) {
         new WareHouseGUI();
     }
@@ -214,5 +227,6 @@ public class WareHouseGUI {
     public JFrame getFr() {
         return fr;
     }
-
+    
+    
 }
